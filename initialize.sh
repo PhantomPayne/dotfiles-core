a8367dcff25c11ff718f6478ea20c734d61bbfa8
Thomas Payne <tpayne.code@gmail.com>
2014-02-02 03:28:01 -0500

    added initialize script

---
+++ initialize.sh
@@ -0,0 +1,11 @@
+# easy install works better cross platform because there are some bimary packages
+easy_install ipython
+
+easy_install pip
+
+pip install virtualenv
+pip install fabric
+
+pip install https://github.com/Lokaltog/powerline
+
+chsh --shell /bin/zsh
